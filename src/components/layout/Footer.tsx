f7b261615e759b3e0be1b87400f6e6ec018e919e
Damian Nolberczyk <dnol.code@gmail.com>
2026-04-13 20:35:55 +0200

    feat: added footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="w-full py-4 text-center ">
+    <footer className="w-full py-8 text-center text-white/30 text-sm border-t border-white/10 bg-dark">
       <span>
-        &copy; {new Date().getFullYear()} Your Name. All rights reserved. |{" "}
-        <a
-          href="mailto:your.email@example.com"
-          className="ml-2 text-blue-600 hover:underline"
+        &copy; {new Date().getFullYear()} Damian Nolberczyk. All rights reserved
+        {/* <a
+          href="mailto:dnol.code@gmail.com"
+          className="ml-2 font-syne text-accent hover:underline"
         >
           Contact
-        </a>
+        </a> */}
       </span>
     </footer>
   );
